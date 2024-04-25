8b182ebf48e41758c1f14e67a5e3b0b850792763
Tessa Murphy <tessamurphy56@gmail.com>
2024-04-25 09:46:18 -0700

    test into index

--- js/main.js
+++ js/main.js
@@ -1,60 +1,89 @@
 
 function Main() {
-
+   
     setColorRadio();
     setToggle();
     //Get arrays from local storage
     var arrDays = JSON.parse(localStorage.getItem("days"));
     var arrTrans = JSON.parse(localStorage.getItem("transformed"))
+    
+    //Check that Arrays exist. If absent, create them.
+    if(!Array.isArray(arrDays)){
+        const days = [];   
+        localStorage.setItem("days", JSON.stringify(days));
+        arrDays = localStorage.getItem("days");
+    } 
+    
+    if(!Array.isArray(arrTrans)){
+        
+        const transformed = [];   
+        localStorage.setItem("transformed", JSON.stringify(transformed));
+        var arrTrans = JSON.parse(localStorage.getItem("transformed")) 
+        
+    } 
+    
     //Set focus and call loadList to create list on page
     document.getElementById("inpTask").focus();
     loadList(arrDays, arrTrans);   
 }
 
 function loadList(arrDays, arrTrans) {
+//Get arrays and create a loop to use when calling createCheckbox.
+//Then call createCheckbox to check or uncheck boxes.
     
     var td = [];
     var nd = new Date().toLocaleDateString('en-us', { weekday:"long"});
+
+    //If arrDays is empty, check whether or not arrTrans is empty.
+    //If arrTrans is not empty, copy arrTrans into the array, td.  
+    if (arrDays.length === 0){
+        
+        if (arrTrans.length === 0){
+            //saySomething.innerHTML="everything's empty";
+            } else {
+                var td = arrTrans;
+            }      
     
-    //Get array and create a loop to call createCheckbox to create each checkbox
-    //Then call createCheckbox to check or uncheck boxes.
-    if(arrDays != ""){
+    //if arrDays is not empty, run through days array extracting everyday 
+    //tasks and current days tasks. Push into the array, td.       
+    } else{
         for (var i = 0; i < arrDays.length; i++){
             let obj = arrDays[i];
-            if(obj["day"] == "Everyday" || obj["day"]== nd){
+            if(obj["day"] == "Everyday" || obj["day"] == nd){
                 td.push(obj)
             } 
         }
-        
-        if(arrTrans !=""){
-            var arrToDo = td.concat(arrTrans);
-        }
-        
-    
-        for(var i = 0; i < arrToDo.length; i++){
-            let obj = arrToDo[i];
+        //If arrTrans isn't empty, concantenate td and arrTrans.
+        if (arrTrans.length != 0){
+            td = td.concat(arrTrans);
+        }   
+    }
+    //If td is an array, call createCheckbox to create task list.
+    if (Array.isArray(td)){
+        for(var i = 0; i < td.length; i++){
+            let obj = td[i];
              createCheckbox(obj)
         }
+
+        //runs through when page loads or reloads and adds checks and bold.  
+        var checkbox = fldHoldList.querySelectorAll("input[type='checkbox']");
+        var labels = document.querySelectorAll(".toDoLabel");
+        
+        var nd = new Date().toLocaleDateString('en-us', { weekday:"long"});
+        for (var i = 0; i < td.length; i++){
+            
+            var obj = td[i];
+            
+            if (checkbox[i].checked == true ){
+                labels[i].classList.add("gray");
+            }
+        
+            if (obj["bold"]=="true"){
+            
+                labels[i].classList.add("bold");
+            }
+        }          
     }
-        
-    //runs through when page loads or reloads and adds checks and bold.  
-    var checkbox = fldHoldList.querySelectorAll("input[type='checkbox']");
-    var labels = document.querySelectorAll(".toDoLabel");
-    
-    var nd = new Date().toLocaleDateString('en-us', { weekday:"long"});
-    for (var i = 0; i < arrToDo.length; i++){
-        
-        var obj = arrToDo[i];
-        
-        if (checkbox[i].checked == true ){
-            labels[i].classList.add("gray");
-        }
-    
-        if (obj["bold"]=="true"){
-           
-            labels[i].classList.add("bold");
-        }
-    } 
 }
 
 function createCheckbox(obj) {
@@ -84,31 +113,27 @@ function createCheckbox(obj) {
 }   
 
 function boxChecked(elt){
-    // Called when a checkbox is clicked
-    // if the checkbox is checked, a class is added to its label
-    // that turns the text gray. If checkbox is unchecked, the class is removed
-    // the value in the object is changed and the array saved to local storage      
- var target = elt;
+// Called when a checkbox is clicked
+// if the checkbox is checked, a class is added to its label
+// that turns the text gray. If checkbox is unchecked, the class is removed
+// the value in the object is changed and the array saved to local storage      
+    var target = elt;
     const value = elt.value;
     var arrDays = JSON.parse(localStorage.getItem("days"));
     var arrTransformed = JSON.parse(localStorage.getItem("transformed"));
-    //var obj = arr.find(obj => obj["todo"] === value);
-    //var indx = arr.indexOf(obj);
     var labels = document.querySelectorAll(".toDoLabel");
     var found = arrDays.includes(arrDays.find(obj => obj["todo"] === value));
+    
     if(found == true){
-       
         var arr = arrDays;
         var name = "days";
         var obj = arr.find(obj => obj["todo"] === value);
         var indx = arr.indexOf(obj); 
-       
     } else {
         var arr = arrTransformed;
         var name = "transformed";
         var obj = arr.find(obj => obj["todo"] === value);
         var indx = arr.indexOf(obj);
-        
     }
     
     for (var i = 0; i < labels.length; i++) {
@@ -120,8 +145,7 @@ function boxChecked(elt){
                 arr.splice(indx, 1, obj);
                 localStorage.setItem(name, JSON.stringify(arr));   
             } else { labels[i].classList.remove("gray");
-           
-            obj["checked"] = "false"
+                obj["checked"] = "false"
                 arr.splice(indx, 1, obj);
                 localStorage.setItem(name, JSON.stringify(arr));  
             }  
@@ -161,8 +185,8 @@ function makeBold(elt){
 }
 
 function getInput() {
-    // Get the input element by its ID
-    //and pass it to createArray 
+// Get the input element by its ID
+//and pass it to createArray 
     
     var inputField = document.getElementById("inpTask"); 
     var value = inputField.value; 
@@ -204,7 +228,6 @@ function clearList() {
 //from the array then reloads the page so the updated list appears.
     
     var transformed = JSON.parse(localStorage.getItem("transformed"));
-    //var days = JSON.parse(localStorage.getItem("days"));
     var check = fldHoldList.querySelectorAll("input[type='checkbox']");
     var label = fldHoldList.querySelectorAll(".toDoLabel");
     var linebreak = fldHoldList.querySelectorAll("br");
@@ -212,43 +235,16 @@ function clearList() {
     for (var i=check.length-1; i >=0; i--){
         
         if(check[i].checked){
-            
             var index = transformed.findIndex(x => x["todo"] === check[i].value);
             
             if (index >= 0){
-                
-                
-                
                 transformed.splice(index, 1);
                 localStorage.setItem("transformed", JSON.stringify(transformed));
-                
             } 
-
-            
             
             fldHoldList.removeChild(check[i]);
-                fldHoldList.removeChild(label[i]);
-                
-                fldHoldList.removeChild(linebreak[i]);
-            /*var str = check[i].value;
-            var idx = transformed.findIndex(x => x["todo"] === str);
-            
-            if(idx >= 0){
-                transformed.splice(idx, 1);
-                localStorage.setItem("transformed", JSON.stringify(transformed));
-                
-                fldHoldList.removeChild(check[i]);
-                fldHoldList.removeChild(label[i]);
-                fldHoldList.removeChild(linebreak[i]);
-            } else {
-                
-                
-
-                
-                
-
-            }*/
-            
+            fldHoldList.removeChild(label[i]);
+            fldHoldList.removeChild(linebreak[i]);
         }
     } 
 }
